9e90c99e345fafe9a8fefa97220e202ba11e64f4
CristianC-A <alvarezrmdcristian@gmail.com>
2026-01-26 22:04:25 -0500

    2-ejercicios

---
+++ Ejercicios-del-1-al-20.py
@@ -0,0 +1,24 @@
+#Aqui resolvere 20 ejercicios basicos para practicar la logica de programacion.
+
+#Ejercicio 1
+# Pedir tu nombre y mostrar un saludo.
+
+# pedirNombre = input("Ingresa tu nombre: ")
+# saludoCompleto = f"Hola {pedirNombre}, bienvenido al sistema"
+# print(saludoCompleto)
+
+#----------------------------------
+
+#Ejercicio2
+#Calculadora de suma: Un sistema contable necesita sumar dos valores ingresados por el usuario.
+
+# primerNumero = int(input("Ingresa el primer numero de la suma: "))
+# segundoNumero = int(input("Ingresa el segundo numero de la suma: "))
+# suma = primerNumero + segundoNumero
+# print(suma)
+
+#----------------------------------
+
+#Ejercicio 3
+#
+
