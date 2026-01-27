eba1ac78be57beaa55ca87af3dcb61c315731101
CristianC-A <111818800+CristianC-A@users.noreply.github.com>
2026-01-27 16:09:21 +0000

    Ejercicio 3

--- Ejercicios-del-1-al-20.py
+++ Ejercicios-del-1-al-20.py
@@ -20,5 +20,35 @@
 #----------------------------------
 
 #Ejercicio 3
-#
+#Operaciones básicas: Una app educativa muestra todas las operaciones entre dos números.
+
+# pedirOperacion = int(input('''Ingresa la operacion matematica que deseas hace, escribiendo el numero de una de de las siguientes opciones:
+# 1: Suma
+# 2: Resta
+# 3: Multiplicación
+# 4: División
+# 1,2,3 o 4: '''))
+
+# numero1 = int(input("Ingresa el primer numero: "))
+# numero2 = int(input("Ingresa el segundo numero: "))
+
+# resultadoSuma = numero1 + numero2
+# resultadoResta = numero1 - numero2
+# resultadoMultiplicacion = numero1 * numero2
+# resultadoDivision = numero1 / numero2
+
+# if pedirOperacion == 1:
+#     print(f"{numero1} + {numero2} = {resultadoSuma}")
+# elif pedirOperacion == 2:
+#     print(f"{numero1} - {numero2} = {resultadoResta}")
+# elif pedirOperacion == 3:
+#     print(f"{numero1} * {numero2} = {resultadoMultiplicacion}")
+# elif pedirOperacion == 4:
+#     print(f"{numero1} / {numero2} = {resultadoDivision}")
+# else: "El dato ingresado es incorrecto"
+
+#-------------------------
+
+
+    
 
