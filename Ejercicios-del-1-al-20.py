4f1cb623f8c9c24604940ea5329ae63464367bc4
CristianC-A <alvarezrmdcristian@gmail.com>
2026-02-01 12:35:24 -0500

    Hasta el ejercicio 10

--- Ejercicios-del-1-al-20.py
+++ Ejercicios-del-1-al-20.py
@@ -52,17 +52,80 @@
 #Ejercicio 4
 #Mayor de edad: Sistema de control de acceso a un sitio para adultos.
 
-pedirEdad = int(input("Ingresa tu edad: "))
+# pedirEdad = int(input("Ingresa tu edad: "))
 
-if pedirEdad >= 18:
-    print(f"Tu edad es {pedirEdad}, Bienvenid@")
-elif pedirEdad < 18 & pedirEdad >= 6:
-    print(f"Tu edad es {pedirEdad}. Lo siento, no puedes ingresar")
-elif pedirEdad > 120:
-    print(f"Tu edad es {pedirEdad}. Imposibleee")
-elif pedirEdad <= 5 & pedirEdad >= 0:
-    print(f'Tu edad es {pedirEdad},aun eres un bebe, no puedes ingresar')
+# if pedirEdad >= 18:
+#     print(f"Tu edad es {pedirEdad}, Bienvenid@")
+# elif pedirEdad < 18 & pedirEdad >= 6:
+#     print(f"Tu edad es {pedirEdad}. Lo siento, no puedes ingresar")
+# elif pedirEdad > 120:
+#     print(f"Tu edad es {pedirEdad}. Imposibleee")
+# elif pedirEdad <= 5 & pedirEdad >= 0:
+#     print(f'Tu edad es {pedirEdad},aun eres un bebe, no puedes ingresar')
 
+#----------------------------------
 
-    
+#Ejercicio 5
+#Conversor Celsius → Fahrenheit
+# la formula para pasar de celcius a fahrenheit es: °F = (°C × 9/5) + 32;
 
+# print("Conversor de temperatura en medidad de celsius a fahrenheit")
+
+# temperaturaEnCelcius = int(input("Ingresa la temperatura que quieres convertir a Fahrenheit: "))
+# temperaturaEnFahrenheit = (temperaturaEnCelcius * 9/5) + 32
+
+# print(f"La temperatura {temperaturaEnCelcius} en grados celcius es igual a {temperaturaEnFahrenheit} en grados Fahrenheit")
+
+#----------------------------------
+
+#Ejercicio 6
+#Área de un rectángulo
+
+# print("Calculadora de area de un rectangulo")
+# baseRectangulo = int(input("Ingresa la base del rectangulo: "))
+# alturaRectangulo = int(input("Ingresa la altura del rectangulo: "))
+# areaRectangulo = (baseRectangulo * alturaRectangulo) / 2
+# print(f"El area del rectangulo es: {areaRectangulo}")
+
+#----------------------------------
+
+#Ejercicio 7
+#Perímetro de un cuadrado
+
+# print("Calculadora de perimetro de un cuadrado")
+# ladoCuadrado = int(input("Ingresa el valor del lado del cuadrado: "))
+# perimetroCuadrado = ladoCuadrado * 4
+# print(f"El perimetro del cuadrado es: {perimetroCuadrado}")
+
+#----------------------------------
+
+#Ejercicio 8
+#Doble de un número
+
+# print("Calculadora de puntos dobles")
+# numeroDePuntos = int(input("Ingresa la cantidad de puntos a calcular: "))
+# puntosDobles = numeroDePuntos * 2
+# print(f"La cantidad de puntos dobles es de : {puntosDobles}")
+
+#----------------------------------
+
+#Ejercicio 9
+#Mitad de un número
+
+# print("Calculadora de la mitad de un numero")
+# numeroACalcular = float(input("Ingresa el numero para calcular su mitad: "))
+# mitadCalculada = float(numeroACalcular / 2)
+# print(f"La mitad de {numeroACalcular} es: {mitadCalculada}")
+
+#----------------------------------
+#Ejercicio 10
+#Calcular IVA (19%)
+
+# print("Calcular IVA")
+# valorProducto = float(input("Ingresa el valor del producto: "))
+# valorIVA = float(valorProducto * 0.19)
+# valorProductoConIVA = int(valorProducto + valorIVA)
+# print(f"El valor total a pagar con IVA es: {valorProductoConIVA}")
+
+#----------------------------------
+#Ejercicio 11
